3ea8dfa842c3d94baad3373d8fa612057c414edb
Joel-stoic <joelimmanual.off@gmail.com>
2026-05-04 19:35:06 +0530

    Added CI

--- backend/test.js
+++ backend/test.js
@@ -1,19 +1,30 @@
 import http from 'k6/http'
-import { sleep } from 'k6'
 
 export const options = {
-  vus: 300,
+  vus: 100,
   duration: '1m',
 }
 
-const code=''
+const codes = ['8yu2yb', 'LuklGT', 'TO06Uq']
 
 export default function () {
   const code = codes[Math.floor(Math.random() * codes.length)]
 
-  http.get(`http://localhost:3000/${code}`, {
-    redirects: 0
-  })
-
-
+  if (Math.random() < 0.8) {
+    // 80% reads (cache-friendly)
+    http.get(`http://34.228.190.116:3000/${code}`, {
+      redirects: 0,
+    })
+  } else {
+    // 20% writes
+    http.post(
+      'http://34.228.190.116:3000/shorten',
+      JSON.stringify({
+        originalUrl: 'https://example.com/' + Math.random(),
+      }),
+      {
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
+  }
 }
